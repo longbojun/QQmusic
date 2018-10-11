c742193d54a80c58a44332a0c45c65016c7e194d
longbojun <longbojun@126.com>
2018-10-11 21:38:57 +0800

    删

--- vue-book/mock/sliders.js
+++
@@ -1,5 +0,0 @@
-module.exports = [
-  'http://img3.imgtn.bdimg.com/it/u=1153339967,3379755410&fm=26&gp=0.jpg',
-  'http://img1.imgtn.bdimg.com/it/u=1131115485,1802446&fm=15&gp=0.jpg',
-  'http://img5.imgtn.bdimg.com/it/u=1550688098,3037215217&fm=26&gp=0.jpg'
-];
